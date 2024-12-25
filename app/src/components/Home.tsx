24b4a6fe8028703e3309fb44115e2500067f3d74
Ed <edouard.forgeau@gmail.com>
2024-12-25 21:08:53 +0100

    chore(pw): keep bootstrapping

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Home: React.FC = () => {
-    return (
-        <div className="home">
-            <h1>Bienvenue sur mon site personnel</h1>
-            <p>Je suis [Votre Nom], un développeur passionné par la technologie et le design.</p>
-            <p>Découvrez mes compétences et mon parcours dans la section <a href="#about">À propos</a>.</p>
-            <p>Consultez mes derniers articles de blog dans la section <a href="#blog">Blog</a>.</p>
-        </div>
-    );
+  const { t } = useTranslation();
+
+  return (
+    <div className="home">
+      <h1>{t('welcome')}</h1>
+      <p>Je suis [Votre Nom], un développeur passionné par la technologie et le design.</p>
+      <p>Découvrez mes compétences et mon parcours dans la section <a href="#about">À propos</a>.</p>
+      <p>Consultez mes derniers articles de blog dans la section <a href="#blog">Blog</a>.</p>
+    </div>
+  );
 };
 
 export default Home;
